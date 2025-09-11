34f617c5ac67c32a00d4f90f31bbf938b29a5ca2
Wei Xiang <chehi716@gmail.com>
2025-09-12 00:39:39 +0800

    Added Features

---
+++ src/main/java/com/jobsuggestbackend/model/Feature.java
@@ -0,0 +1,53 @@
+package com.jobsuggestbackend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table( name = "feature" )
+public class Feature {
+    @Id
+    @GeneratedValue
+    private long Id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String value;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // Many posts -> one user
+    @JoinColumn(name = "roles_id", nullable = false) // Foreign key column
+    private Role role;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
